62d8fadfe36c3a3271f11e3333d24b4864af38cc
Ali Amor <alibenamor30@gmail.com>
2023-05-07 22:50:38 +0100

    Theme management

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,17 @@
 <script>
   import Header from "./Header.svelte";
   import "./styles.css";
+  import { onMount, setContext } from "svelte";
+  import { initTheme } from "$lib/services/theme";
+  import { writable} from "svelte/store";
+
+  const themeStore = writable();
+  setContext("theme", themeStore);
+  onMount(() => {
+    const theme = initTheme();
+    themeStore.set(theme);
+  });
+
 </script>
 
 <div class="app">
