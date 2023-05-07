8a33b257d7bf3c49e5d01fca9f3901b7cf2402df
Ali Amor <alibenamor30@gmail.com>
2023-05-07 22:49:19 +0100

    Theme management components

---
+++ src/lib/components/ThemeButton.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+  import { getContext } from "svelte";
+  import { setTheme } from "../services/theme";
+  export let themeName = "light";
+  export let themeDisplay = "Light";
+  export let selected = false;
+  export let className = "";
+
+  const themeStore = getContext("theme");
+</script>
+
+<button
+  class="{className} outline-base-content overflow-hidden rounded-lg text-left [&amp;_svg]:visible"
+  data-set-theme={themeName}
+  data-act-class="[&amp;_svg]:visible"
+  on:click={() => {
+    setTheme(themeName);
+    themeStore.set(themeName);
+    }}
+  ><div
+    data-theme={themeName}
+    class="bg-base-100 text-base-content w-full cursor-pointer font-sans"
+  >
+    <div class="grid grid-cols-5 grid-rows-3">
+      <div
+        class="col-span-5 row-span-3 row-start-1 flex gap-2 py-3 px-4 items-center"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          class="w-3 h-3 {selected ? '' : 'invisible'}"
+          ><path
+            d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"
+          /></svg
+        >
+        <div class="flex-grow text-sm font-bold">{themeDisplay}</div>
+        <div class="flex flex-shrink-0 flex-wrap gap-1 h-full">
+          <div class="bg-primary w-2 rounded" />
+          <div class="bg-secondary w-2 rounded" />
+          <div class="bg-accent w-2 rounded" />
+          <div class="bg-neutral w-2 rounded" />
+        </div>
+      </div>
+    </div>
+  </div>
+  </button>
---
+++ src/lib/components/ThemeSelectButton.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { getContext } from "svelte";
+  import ThemeButton from "./ThemeButton.svelte";
+  import PaintIcon from "./icons/Paint.svelte";
+
+  const themeStore = getContext("theme");
+  
+</script>
+
+<div class="dropdown">
+  <button class="btn btn-circle btn-ghost">
+    <PaintIcon />
+  </button>
+  <div class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-56 right-0">
+    <ThemeButton selected={ $themeStore === 'acid'} className="my-2" themeName="acid" themeDisplay="Acid" />
+    <ThemeButton selected={ $themeStore === 'emerald'} className="my-2" themeName="emerald" themeDisplay="Emerald" />
+    <ThemeButton selected={ $themeStore === 'night'} className="my-2" themeName="night" themeDisplay="Night" />
+    <ThemeButton selected={ $themeStore === 'synthwave'} className="my-2" themeName="synthwave" themeDisplay="Synth Wave" />
+    <ThemeButton selected={ $themeStore === 'forest'} className="my-2" themeName="forest" themeDisplay="Forest" />
+    <ThemeButton selected={ $themeStore === 'business'} themeName="business" themeDisplay="Business" />
+  </div>
+</div>
