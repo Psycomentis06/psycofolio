c2ceea65f8c2b6819b871a75be4d1acab3eab8b0
Ali Amor <alibenamor30@gmail.com>
2023-05-10 07:20:25 +0100

    Login component

---
+++ src/lib/components/Login.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+    import EyeSlashIcon from "./icons/EyeSlash.svelte";
+    import EyeIcon from "./icons/Eye.svelte";
+    import LoginIcon from "./icons/Login.svelte";
+    let passwordVisible = false
+
+    function togglePasswordVisibility() {
+        passwordVisible = !passwordVisible
+    }
+</script>
+<label for="login-modal" class="btn btn-ghost btn-circle">
+    <LoginIcon />
+</label>
+
+<!-- Put this part before </body> tag -->
+<input type="checkbox" id="login-modal" class="modal-toggle" />
+<div class="modal">
+  <div class="modal-box relative bg-base-100">
+    <label for="login-modal" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+    <h3 class="text-xl font-bold text-center">Login (only for admin)</h3>
+    <div class="login-form my-6 flex items-center justify-center flex-col">
+        <div class="form-control w-full max-w-xs">
+            <label class="label" for="username-input">
+              <span class="label-text">Username</span>
+            </label>
+            <input id="username-input" type="text" placeholder="Username" class="input input-bordered w-full max-w-xs" />
+          </div>
+
+        <div class="form-control w-full max-w-xs relative">
+            <label class="label" for="password-input">
+              <span class="label-text">Password</span>
+            </label>
+            <input id="password-input" type="{passwordVisible ? 'text': 'password'}" placeholder="Password" class="input input-bordered w-full max-w-xs" />
+            <button on:click={() => togglePasswordVisibility() } class="swap absolute top-12 right-2">
+              <div class="{passwordVisible ? 'swap-off' : 'swap-on'}">
+                <EyeIcon />
+              </div>
+              <div class="{passwordVisible ? 'swap-on' : 'swap-off'}">
+                <EyeSlashIcon />
+              </div>
+            </button>
+          </div>
+
+          <div class="my-5">
+            <button class="btn btn-primary btn-wide">Submit</button>
+          </div>
+    </div>
+  </div>
+</div>
