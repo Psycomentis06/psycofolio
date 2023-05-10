ca94a71288a41b03594647a824052c6a238cacd7
Ali Amor <alibenamor30@gmail.com>
2023-05-10 08:14:29 +0100

    Set language and update ui

--- src/lib/components/LanguageSelectButton.svelte
+++ src/lib/components/LanguageSelectButton.svelte
@@ -1,13 +1,25 @@
 <script lang="ts">
     import { onMount } from "svelte";
-    import { getAvailableLanguages, getActiveLanguage } from '../services/language'
+    import { getAvailableLanguages, getActiveLanguage, setLanguage } from '../services/language'
     import LanguageIcon from "./icons/Language.svelte";
+    import { page } from '$app/stores'
+  import { goto } from "$app/navigation";
 
     let languages = getAvailableLanguages();
     let activeLanguage: string
     onMount(() => {
       activeLanguage = getActiveLanguage();
     });
+
+    function changeLanguage(lang: string) {
+      setLanguage(lang, () => {
+        let locale = $page.url.pathname.split('/')
+        locale[1] = lang
+        const localePath = locale.join('/')
+        activeLanguage = lang
+        goto(localePath, { replaceState: true })
+      }) 
+    }
 </script>
 <div title="Change Language" class="dropdown dropdown-end">
   <button class="btn btn-ghost btn-circle">
@@ -19,7 +31,7 @@
     <ul class="menu menu-compact gap-1 p-3">
         {#each languages as lang}
       <li>
-        <button class="{activeLanguage === lang.code ? 'active' : ''} flex rounded-box p-2 w-full text-left"
+        <button on:click={() => changeLanguage(lang.code)} class="{activeLanguage === lang.code ? 'active' : ''} flex rounded-box p-2 w-full text-left"
           ><img
             loading="lazy"
             width="20"
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,24 +3,33 @@
   import "./styles.css";
   import { onMount, setContext } from "svelte";
   import { initTheme } from "$lib/services/theme";
-  import { setLanguage, getActiveLanguage } from '$lib/services/language'
-  import { writable} from "svelte/store";
+  import { setLanguage, getActiveLanguage, isAvailableLanguage } from '$lib/services/language'
+  import { get, writable} from "svelte/store";
+  import { page } from '$app/stores'
+  import { goto } from '$app/navigation'
   import type { LayoutData } from "./$types";
 
   export let data: LayoutData;
   const themeStore = writable();
   setContext("theme", themeStore);
   onMount(() => {
+    
     // Set theme
     const theme = initTheme();
     themeStore.set(theme);
     // Set lang
-    if (window.location.pathname === "/") {
+    const pathname = $page.url.pathname
+    if (pathname === "/") {
       setLanguage(getActiveLanguage());
-      window.location.href = `/${getActiveLanguage()}`;
+      goto(`/${getActiveLanguage()}`, { replaceState: true });
     } else {
-      if (data.lang) {
-        setLanguage(data.lang);
+      if (data.lang === undefined || data.lang === null) {
+        setLanguage(getActiveLanguage());
+      } else if (isAvailableLanguage(data.lang)) {
+        setLanguage(data.lang)
+      } else {
+        setLanguage(getActiveLanguage());
+        goto(`/${getActiveLanguage()}/${pathname}`, { replaceState: true });
       }
     }
   });
