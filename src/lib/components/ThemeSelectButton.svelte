a69bc6b9df4b5f73dd78d3a0dd37ac76ab0066a6
Ali Amor <alibenamor30@gmail.com>
2023-05-10 07:19:22 +0100

    dynamic theme selection

--- src/lib/components/ThemeSelectButton.svelte
+++ src/lib/components/ThemeSelectButton.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { getContext } from "svelte";
+  import { getAvailableThemes } from '../services/theme'
   import ThemeButton from "./ThemeButton.svelte";
   import PaintIcon from "./icons/Paint.svelte";
 
@@ -12,11 +13,8 @@
     <PaintIcon />
   </button>
   <div class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-56 right-0">
-    <ThemeButton selected={ $themeStore === 'acid'} className="my-2" themeName="acid" themeDisplay="Acid" />
-    <ThemeButton selected={ $themeStore === 'emerald'} className="my-2" themeName="emerald" themeDisplay="Emerald" />
-    <ThemeButton selected={ $themeStore === 'night'} className="my-2" themeName="night" themeDisplay="Night" />
-    <ThemeButton selected={ $themeStore === 'synthwave'} className="my-2" themeName="synthwave" themeDisplay="Synth Wave" />
-    <ThemeButton selected={ $themeStore === 'forest'} className="my-2" themeName="forest" themeDisplay="Forest" />
-    <ThemeButton selected={ $themeStore === 'business'} themeName="business" themeDisplay="Business" />
+    {#each getAvailableThemes() as theme }
+      <ThemeButton selected={ $themeStore === theme.code} className="my-2" themeName={theme.code} themeDisplay={theme.name} />
+    {/each}
   </div>
 </div>
