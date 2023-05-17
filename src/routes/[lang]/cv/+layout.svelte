64f0c514bf099a3c122b09bac50ec4ed84fdaa35
Ali Amor <alibenamor30@gmail.com>
2023-05-17 11:22:49 +0100

    CV pages and routing

---
+++ src/routes/[lang]/cv/+layout.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import ProfileDrawer from './ProfileDrawer.svelte';
+    import { page } from '$app/stores'
+
+    $: pathname = $page.url.pathname
+</script>
+<section>
+  <div class="flex flex-row font-firasan">
+    <div class="sidebar w-[300px] shadow-lg bg-base-200 p-4 rounded-lg mx-2">
+        <ProfileDrawer />
+    </div>
+      <div class="w-full mx-2 p-4 bg-base-200 rounded-lg shadow-lg">
+        <div class="tabs mb-5 w-full items-center justify-center">
+            <a href='education' class=" { pathname.endsWith('education') ? 'tab-active' : ''} tab tab-md tab-bordered">Education</a> 
+            <a href='experience' class=" { pathname.endsWith('experience') ? 'tab-active' : ''} tab tab-md tab-bordered">Experience</a> 
+            <a href='skills' class=" { pathname.endsWith('skills') ? 'tab-active' : ''} tab tab-md tab-bordered">Skills</a>
+            <a href='projects' class="{ pathname.endsWith('projects') ? 'tab-active' : ''} tab tab-md tab-bordered">Projects</a>
+            <a href='hobbies' class="{ pathname.endsWith('hobbies') ? 'tab-active' : ''} tab tab-md tab-bordered">Hobbies</a>
+            <a href='languages' class="{ pathname.endsWith('languages') ? 'tab-active' : ''} tab tab-md tab-bordered">Languages</a>
+          </div>
+        <slot />
+      </div>
+  </div>
+</section>
---
+++ src/routes/[lang]/cv/ProfileDrawer.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import DefaultAvatarImg from '$lib/assets/images/avatar.png';
+  import SocialMediaLink from '$lib/components/SocialMediaLink.svelte';
+  import { SocialMediaNames } from '$lib/services/social_media';
+</script>
+<div class="profile-drawer">
+    <div class="avatar-div flex items-center justify-center">
+        <div class="avatar">
+            <div class="w-24 rounded-full ring-4 ring-primary ">
+              <img src={DefaultAvatarImg} alt="" />
+            </div>
+          </div>
+    </div>
+    <div class="my-4 text-center">
+        <h1 class="text-xl my-2">Username</h1>
+        <h2 class="text-sm my-2">user's job</h2>
+        <h2 class="text-sm my-2"><a href="mailto:email@mail.com?subject = Contact&body = :)">email@mail.com</a></h2>
+        <address class="text-sm my-2">user's address</address>
+        <a href="tel:00-00-00-00" class="text-sm my-2">+1 00 00 00 00</a>
+    </div>
+    <div class="divider"></div>
+    <div class="social-media font-sans flex justify-around items-center flex-wrap" style="gap: 10px;">
+        <SocialMediaLink socialMediaName={SocialMediaNames.REDDIT} username="user" />
+        <SocialMediaLink socialMediaName={SocialMediaNames.DISCORD} username="user" />
+        <SocialMediaLink socialMediaName={SocialMediaNames.STACKOVERFLOW} username="user" />
+    </div>
+</div>
